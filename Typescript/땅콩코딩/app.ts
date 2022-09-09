b08ce67547dc4ab464fb3d4df915c8d7f6cee51d
Awesomeyelim <uiop01900@gmail.com>
2022-09-09 18:40:51 +0900

    🔥004 타입명시

--- Typescript/땅콩코딩/app.ts
+++ Typescript/땅콩코딩/app.ts
@@ -1,20 +1,42 @@
-// 003. 예제 - 1
-let a = 10 
-// a = '10' 
+// // 003. 예제 - 1
+// let a = 10
+// // a = '10'
 
-// 003. 예제 - 2
-let human = {
-    name : 'Yelim',
-    goal : 'Fullstack developer',
-    nation : 'Korean',
-    age : 28,
-    code: function(){
-        console.log('dont know what to do')
-    }
-}
-// human.name = 10
+// // 003. 예제 - 2
+// let human = {
+//     name : 'Yelim',
+//     goal : 'Fullstack developer',
+//     nation : 'Korean',
+//     age : 28,
+//     code: function(){
+//         console.log('dont know what to do')
+//     }
+// }
+// // human.name = 10
 
-// 003. 예제 - 3
-function youngeryelim(lostage){
-    return 28 - lostage;
-}
+// // 003. 예제 - 3
+// function youngeryelim(lostage){
+//     return 28 - lostage;
+// }
+
+// 004. 예제 - 1
+
+let myname: string = "yelim";
+let age: number = 28;
+let gender: string = "female";
+let hobby: string = "playingguitar";
+let married: boolean = false;
+
+// 004. 예제 - 2
+
+function aboutyelim(age: number): {
+    myname: string;
+    age: number;
+    gender: string;
+    hobby: string;
+    married: boolean;
+} {
+    return { myname, age, gender, hobby, married }
+} 
+
+
