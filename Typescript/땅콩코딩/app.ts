7405d861d429bb9f6b09f5e9d0d256b723b720ae
Awesomeyelim <uiop01900@gmail.com>
2022-09-09 20:18:46 +0900

    🔥005

--- Typescript/땅콩코딩/app.ts
+++ Typescript/땅콩코딩/app.ts
@@ -3,7 +3,7 @@
 // // a = '10'
 
 // // 003. 예제 - 2
-// let human = {
+// let aboutme = {
 //     name : 'Yelim',
 //     goal : 'Fullstack developer',
 //     nation : 'Korean',
@@ -12,7 +12,7 @@
 //         console.log('dont know what to do')
 //     }
 // }
-// // human.name = 10
+// // aboutme.name = 10
 
 // // 003. 예제 - 3
 // function youngeryelim(lostage){
@@ -21,22 +21,107 @@
 
 // 004. 예제 - 1
 
-let myname: string = "yelim";
-let age: number = 28;
-let gender: string = "female";
-let hobby: string = "playingguitar";
-let married: boolean = false;
+// let myname: string = "yelim";
+// let age: number = 28;
+// let gender: string = "female";
+// let hobby: string = "playingguitar";
+// let married: boolean = false;
 
 // 004. 예제 - 2
 
-function aboutyelim(age: number): {
-    myname: string;
-    age: number;
-    gender: string;
-    hobby: string;
-    married: boolean;
-} {
-    return { myname, age, gender, hobby, married }
-} 
+// function aboutyelim(age: number): {
+//   myname: string;
+//   age: number;
+//   gender: string;
+//   hobby: string;
+//   married: boolean;
+// } {
+//   return {
+//     myname: "yelim",
+//     age: 28,
+//     gender: "female",
+//     hobby: "playingguitar",
+//     married: false,
+//   };
+// }
 
+// 005. 예제 - 1
+
+// interface Aboutme {
+//     myname: string;
+//     age?: number;
+//     gender: string;
+//     hobby: string;
+//     married: boolean;
+//   }
+
+//   function aboutyelim(age: number): Aboutme {
+//     return {
+//       myname: "yelim",
+//       gender: "female",
+//       hobby: "playingguitar",
+//       married: false,
+//     };
+//   }
+
+// 005. 예제 - 2
+
+// interface Aboutme {
+//   myname: string;
+//   age: number;
+//   gender: string;
+//   hobby: string;
+//   married: boolean;
+// }
+
+// function aboutyelim(age: number): Aboutme {
+//   return {
+//     myname: "yelim",
+//     age: 28,
+//     gender: "female",
+//     hobby: "playingguitar",
+//     married: false,
+//   };
+// }
+
+// const who = {
+//     myname: "yelim",
+//     age: 28,
+//     gender: "female",
+//     hobby: "playingguitar",
+//     married: false,
+//   };
+
+// function aboutyelim2(aboutme: Aboutme): void {}
+
+// aboutyelim2(who);
+
+// 005. 예제 - 3
+
+interface Aboutme {
+ readonly myname: string;
+  age: number;
+  gender: string;
+  hobby: string;
+  married: boolean;
+  thisismethod?(comment: string): string;
+  //   thisismethod? : (comment : string) => string;
+}
+
+const who = {
+  myname: "yelim",
+  age: 28,
+  gender: "female",
+  hobby: "playingguitar",
+  married: false,
+};
+
+function aboutyelim(age: number): Aboutme {
+  return who;
+}
+
+function aboutyelim2(aboutme: Aboutme): void {
+}
+
+aboutyelim2(who);
 
