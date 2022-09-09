70df2c7c7169d021737ba6e6b0cb29d7139e7b4a
Awesomeyelim <uiop01900@gmail.com>
2022-09-09 17:28:55 +0900

    🔥003 타입추론

---
+++ Typescript/땅콩코딩/app.ts
@@ -0,0 +1,20 @@
+// 003. 예제 - 1
+let a = 10 
+// a = '10' 
+
+// 003. 예제 - 2
+let human = {
+    name : 'Yelim',
+    goal : 'Fullstack developer',
+    nation : 'Korean',
+    age : 28,
+    code: function(){
+        console.log('dont know what to do')
+    }
+}
+// human.name = 10
+
+// 003. 예제 - 3
+function youngeryelim(lostage){
+    return 28 - lostage;
+}
