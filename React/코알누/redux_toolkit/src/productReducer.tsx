4ac3826129bd9989aff6915b996a387f7f4af7f8
Awesomeyelim <uiop01900@gmail.com>
2022-09-14 08:36:46 +0900

    📚002내장함수

---
+++ React/코알누/redux_toolkit/src/productReducer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Reducer } from '@reduxjs/toolkit';
+
+
+let initialState = {
+    productList: [],
+    selectedItem: null,
+}
+
+function productReducer(state = initialState, action){
+
+}
+
+export default productReducer
