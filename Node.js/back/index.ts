f21c162037cb0960c7c3d5041c52c4309d8f17cc
AwesomeYelim <uiop01900@gmail.som>
2023-06-28 21:38:19 +0900

    node

--- Node.js/back/index.ts
+++ Node.js/back/index.ts
@@ -1,9 +1,51 @@
 import * as express from "express";
+import * as morgan from "morgan";
+import * as cors from "cors";
+import * as cookieParser from "cookie-parser";
+import helmet from "helmet";
+import * as expressSession from "express-session";
+import * as dotenv from "dotenv";
+import * as passport from "passport";
+import * as hpp from "hpp";
 
+dotenv.config();
 const app = express();
 const prod: boolean = process.env.NODE_ENV === "production";
 
 app.set("port", prod ? process.env.PORT : 3056);
+
+if (prod) {
+  app.use(hpp());
+  app.use(helmet());
+  app.use(morgan("combined"));
+  app.use(cors({ origin: /nodebird\.com$/, credentials: true }));
+} else {
+  app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
+}
+
+app.use("/", express.static("uploads"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET!,
+    cookie: {
+      httpOnly: true,
+      secure: false, // https -> true
+      domain: prod ? ".nodebird.com" : undefined,
+    },
+    name: "rnbck",
+  })
+);
 
 app.get("/", (req, res, next) => {
   res.send("백앤드 정상임");
