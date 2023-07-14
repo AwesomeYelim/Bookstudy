3f3022363ef5e7d1a2abda4fbda0015e215ab55d
AwesomeYelim <uiop01900@gmail.com>
2023-07-14 17:44:58 +0900

    react

--- i18next/trans/public/locales/sheetUploader.js
+++ i18next/trans/public/locales/sheetUploader.js
@@ -1,28 +1,24 @@
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
 /**
  * 1. 컴포넌트 : client\src\locales\mutiTranslateSheet.js
  * 2. 작성일 : 2023.05.23 / 14시 09분 06초
  * 3. 작성자 : 홍예림
- * 4. 설명 : googleSheet 연동 및 Json 파일 => googleSheet(수정사항만 반영) 자동화
+ * 4. 설명 : googleSheet 연동 및 googleSheet => Json 파일(추가 및 수정사항 반영) 자동화
  */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-import-assign
 const fs = require('fs');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const prettier = require('prettier');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const prettier = require('prettier');
 const dotEnv = require('dotenv');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ko = require('../ko/translation.json');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ja = require('../ja/translation.json');
+const langs = [require('../ko/translation.json'), require('../ja/translation.json')]; // :ADD 언어 추가시
 
 // google Spreadsheet id
 const doc = new GoogleSpreadsheet('15URUcI8eRzi_iTZC7tZhI6BKMSJvmk5wnhSlXwtfch4');
 
-// Json => google-spreadsheet 연동
-async function Uploader() {
+// google-spreadsheet => Json 연동
+async function Downloader() {
   // 서버실행 이전에 env path -> 개발환경으로 설정, env file 을 호출
   dotEnv.config({ path: '.env.development', override: true });
 
@@ -32,41 +28,73 @@ async function Uploader() {
   });
   await doc.loadInfo();
   const sheet1 = doc.sheetsByIndex[0]; // WorkSheet1 선택
-  const rows = await sheet1.getRows(); // 전체 row
-  const sheetHeader = doc.sheetsById[0].headerValues; // WorkSheet1 header -> sheet의 header 가 기준
-  const header = [...sheetHeader].shift(); // 'ss_numbering' 부분 제외
+  const rows = await sheet1.getRows();
+  const sheetHeader = doc.sheetsById[0].headerValues; // WorkSheet1 header
+  const trimHeader = [...sheetHeader].splice(1, sheetHeader.length - 1); // 'ss_numbering' 부분 제외
+  // console.log(sheetHeader); // :FIXME api headerValues 가 [] 로 올때가 있음 - 추후 수정
+  const langArr = [...langs];
 
-  // sheetHeader.forEach(
-  //   (lang) =>
-  //     rows.forEach((row) => {
-  //       console.log(row[lang]);
-  //     }) // sheet에 있는 ko, ja
-  // );
+  const headers = langArr.map((langVar, i) => {
+    return { [trimHeader[i]]: langVar };
+  });
+  headers.forEach((_, i) => {
+    const sheetLang = {};
+    let jsonLang = {};
+    const [strLang, objLang] = [trimHeader[i], langArr[i]];
+    rows.forEach((row) => {
+      // 각 header 부분의 value 를 객체 형식으로 만들어줌
+      sheetLang[row.ss_numbering] = row[strLang];
+    });
 
-  await sheet1.addRow({ ss_numbering: 'ss_7', ko: '자산 관리', ja: '資産管理' });
+    // 각각의 언어 파일내 추가된 key의 여부를 확인
+    const addKeys = Object.keys(sheetLang).filter((key) => !Object.keys(objLang).includes(key));
+
+    // value 값이 수정된 경우
+    const differValues = Object.entries(sheetLang).filter(([key, value]) => objLang[key] !== value);
+
+    // // key 또는 value 값이 삭제된 경우
+    // const deleteProps = Object.entries(objLang).filter(
+    //   ([key, value]) => !Object.keys(sheetLang).includes(key) || !Object.values(sheetLang).includes(value)
+    // );
+
+    // 추가된 키가 있거나 && 수정사항 발생시 && 삭제된 사항이 있을시 파일 수정 실행
+    if ((addKeys && addKeys.length) || (differValues && differValues.length)) {
+      jsonLang = { ...sheetLang };
+      // // 삭제된 key 가 있는경우
+      // if (deleteProps.length) {
+      //   deleteProps.forEach(([key]) => {
+      //     delete jsonLang[key];
+      //   });
+      // }
+      // 추가된 키가 있는경우
+      if (addKeys.length) {
+        addKeys.forEach((key) => {
+          jsonLang[key] = sheetLang[key];
+        });
+      }
+      // 수정된 값이 있는경우
+      if (differValues.length) {
+        differValues.forEach(([key, value]) => {
+          jsonLang[key] = value;
+        });
+      }
+
+      fs.writeFile(
+        `src/locales/${strLang}/translation.json`,
+        // prettier.format(JSON.stringify(jsonLang), { filepath: `src/locales/${strLang}/translation.json` }),
+        JSON.stringify(jsonLang, null, 3),
+        (err) => console.log(err)
+      );
+
+      // literal type 추론 적용
+      if (strLang === 'ko') {
+        fs.writeFile(
+          `src/common/hooks/copyKo.ts`,
+          `export const fixko = ${JSON.stringify(jsonLang, null, 3)} as const`,
+          (err) => console.log(err)
+        );
+      }
+    }
+  });
 }
-Uploader();
-// async function Run() {
-//   await doc.useServiceAccountAuth(credentials);
-//   await doc.loadInfo();
-//   const sheet1 = doc.sheetsByIndex[0]; // WorkSheet 선택
-//   const result = await sheet1.getRows();
-//   await sheet1.addRows([
-//     { 'ss_numbering': 'ss_numbering', ko: 'ko', ja: 'ja', en: 'en' },
-//     { ko: '자산 설정', ja: '資産設定', en: 'asset setup' },
-//     { ko: '탐지 내역', ja: '検出履歴' },
-//     { 'ss_numbering': 'ss_3', ko: '자산 관리', ja: '資産管理' },
-//   ]);
-//   result[2].ko = '추가되는 ko'; // 수정만 가능함
-//   await result[2].save(); // save updates
-
-//   // await sheet1.insertDimension('ROWS', { startIndex: 1, endIndex: 2 }, false);
-//   // await sheet1.addRows([{ ㅁㄴㅇㅁㄴㅇ: 'ko' }]);
-//   // await doc.addSheet({ title: '시트2' }); // 시트 추가
-//   // await doc.updateProperties({ title: 'ssrTranslation' , }); // 구글 시트 이름 변경
-//   // await doc.addSheet({ headerValues: ['', 'name', 'email'] }); // 시트 추가 및 header 설정
-//   // eslint-disable-next-line no-underscore-dangle
-//   console.log(result);
-// }
-
-// Run();
+Downloader();
