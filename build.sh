bb9e9d4e0a3d9dfcd32f26a06defed00772e0232
Awesomeyelim <uiop01900@gmail.com>
2023-09-10 11:51:39 +0900

    mdInterlock📌

--- build.sh
+++ build.sh
@@ -5,4 +5,18 @@ mkdir output # 최상위 root에 output 이라는 파일을 만들겠다.
 # cp -R ./*/*/*.md ./output 
 # find ./* -name *.md | cpio -pdm output # 디렉토리까지 복사
 
-find ./* -type f -name "b_*.md" ! -path "*/node_modules/*" -exec cp {} output \;
+# find ./* -type f -name "b_*.md" ! -path "*/node_modules/*" -exec cp {} output \;
+# rename output/b_*.md output/?_*.md
+
+# find ./* -type f -name "b_*.md" ! -path "*/node_modules/*" -exec sh -c 'cp "$1" "output/$(basename "$1" | sed "s/b_//")"' _ {} \; # 'b_' 이부분만 삭제해줌
+find ./* -type f -name "b_*.md" ! -path "*/node_modules/*" -exec sh -c 'cp "$1" "output/$(basename "$1" | awk -F_ "{print \$NF}")"' _ {} \; # '_'기준으로 split 한뒤 마지막 요소값만 사용 
+# counter=1
+# find ./* -type f -name "b_*.md" ! -path "*/node_modules/*" -exec sh -c 'cp "$1" "output/$counter_$(basename "$1" | awk -F_ "{print \$NF}")"' _ {} \; -exec sh -c 'counter=$((counter+1))' \;
+
+# index=1
+# find ./* -type f -name "b_*.md" ! -path "*/node_modules/*" -print | while read -r file; do
+#     cp "$file" "output/${index}_$(basename "$file" | awk -F_ "{print \$NF}")"
+#     index=$((index+1))
+# done
+
+
