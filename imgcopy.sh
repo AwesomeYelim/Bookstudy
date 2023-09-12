1bef7aba338e199b7e3b2b9eb4e5641ef281532f
AwesomeYelim <uiop01900@gmail.com>
2023-09-12 17:01:34 +0900

    img_interock✨

---
+++ imgcopy.sh
@@ -0,0 +1,10 @@
+
+
+# img 디렉토리 생성
+mkdir mdimg
+
+# 확장자 여러버전 복사 나머지 네이밍 규칙은 위와 같음
+find ./* -type f \( -name "b_*.jpg" -o -name "b_*.jpeg" -o -name "b_*.png" \) ! -path "*/node_modules/*"  -exec sh -c 'cp "$1" "mdimg/$(basename "$1" | awk -F_ "{print \$NF}")"' _ {} \;
+
+
+
