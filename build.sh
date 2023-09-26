a5b086b6f4e8a9cb2dba6d6564abc7fde0e35615
AwesomeYelim <uiop01900@gmail.com>
2023-09-26 18:46:06 +0900

    common_fn🐬🐬

--- build.sh
+++ build.sh
@@ -15,12 +15,6 @@ mkdir output # 최상위 root에 output 이라는 파일을 만들겠다.
 # 'b_' 이부분만 삭제 및 '_'기준으로 split 해준 배열의 마지막 요소 값 사용
 find ./* -type f -name "b_*.md" ! -path "*/node_modules/*" -exec sh -c 'cp "$1" "output/$(basename "$1" | awk -F_ "{print \$NF}")"' _ {} \; # '_'기준으로 split 한뒤 마지막 요소값만 사용 
 
-# img 디렉토리 생성
-mkdir mdimg
-
-# 확장자 여러버전 복사 나머지 네이밍 규칙은 위와 같음
-find ./* -type f \( -name "b_*.jpg" -o -name "b_*.jpeg" -o -name "b_*.png" \) ! -path "*/node_modules/*"  -exec sh -c 'cp "$1" "mdimg/$(basename "$1" | awk -F_ "{print \$NF}")"' _ {} \;
-
 
 # 만약 indexing 값이 필요하다면 이것을 사용
 # index=1
