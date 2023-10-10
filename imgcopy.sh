64453024002a77af933d9d863b225bc690c336c9
AwesomeYelim <uiop01900@gmail.com>
2023-10-10 18:38:32 +0900

    mdImg🐬

--- imgcopy.sh
+++ imgcopy.sh
@@ -4,7 +4,7 @@
 mkdir mdimg
 
 # 확장자 여러버전 복사 나머지 네이밍 규칙은 위와 같음
-find ./* -type f \( -name "b_*.jpg" -o -name "b_*.jpeg" -o -name "b_*.png" \) ! -path "*/node_modules/*"  -exec sh -c 'cp "$1" "mdimg/$(basename "$1" | awk -F_ "{print \$NF}")"' _ {} \;
+find ./* -type f \( -name "b_*.jpg" -o -name "b_*.jpeg" -o -name "b_*.png" -o -name "b_*.gif"\) ! -path "*/node_modules/*"  -exec sh -c 'cp "$1" "mdimg/$(basename "$1" | awk -F_ "{print \$NF}")"' _ {} \;
 
 
 
