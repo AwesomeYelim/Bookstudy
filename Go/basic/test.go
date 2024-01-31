e157f4638dcf47938129ad26ec9638ad14218987
AwesomeYelim <uiop01900@gmail.com>
2024-01-31 20:49:47 +0900

    gogo씽

--- Go/basic/test.go
+++ Go/basic/test.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+// 고루틴이 모두 끝날 때 까지 기다리기 위해 앞에서는 time.Sleep
+func job(s string) {
+	for i := 0; i < 3; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s, i)
+	}
+} // 다른 프로그램(cpu에서 수행되는 다른 작업)은 고루틴을 기다려 주지 않음
 func main() {
-	var x []int = []int{1, 2, 3, 4, 5, 6}
-	b := append(x, 7, 9)
-	fmt.Println(cap(b)) // len(x) + cap(x) 
+	go job("넷플릭스 보기")
+	go job("네일하기")
+	time.Sleep(1000 * time.Millisecond)
+
 }
