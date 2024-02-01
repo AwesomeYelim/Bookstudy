f60e57da81578a1a0b6c26e010a6e50a49b95d5c
AwesomeYelim <uiop01900@gmail.com>
2024-02-01 21:13:13 +0900

    waitgroup

--- Go/basic/test.go
+++ Go/basic/test.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 // 고루틴이 모두 끝날 때 까지 기다리기 위해 앞에서는 time.Sleep
 func job(s string) {
+	defer wg.Done()
+
 	for i := 0; i < 3; i++ {
-		time.Sleep(100 * time.Millisecond)
 		fmt.Println(s, i)
 	}
 } // 다른 프로그램(cpu에서 수행되는 다른 작업)은 고루틴을 기다려 주지 않음
 func main() {
+	fmt.Println("처음에..")
+	wg.Add(1)
 	go job("넷플릭스 보기")
+	wg.Add(1)
 	go job("네일하기")
-	time.Sleep(1000 * time.Millisecond)
+	wg.Wait()
 
 }
